ee93fff2bc0f015da033a6c29ff6d36640cf483f
hudyakow.dev <108136177+hudyakow@users.noreply.github.com>
2023-07-31 06:35:15 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+function myFunction() {
+    var para = document.getElementById("myDropdown");
+    para.classList.toggle("show");
+
+    var up = document.getElementById("up");
+    up.classList.toggle("rotate");
+}
+
+function myFunctin() {
+    var para = document.getElementById("myDropdown1");
+    para.classList.toggle("show");
+
+    var up = document.getElementById("up");
+    up.classList.toggle("rotate");
+}
+/*$(document).ready(function(){
+	$('#nav-icon2').click(function(){
+		$(this).toggleClass('open');
+	});
+
+});
+
+
+function buttonclick()    
+{    
+    const menuList = document.getElementById("menu-bar");
+    const menu = document.getElementById("mobile-menu") 
+    if (menuList.className == "menuOff" && menu.className == "blurOn")    
+    {    
+        menu.className = "blurOff";    
+        menuList.className = "menuOn";    
+    } else    
+    {    
+        menu.className = "blurOn"
+        menuList.className = "menuOff";    
+    }    
+}*/
+
+
+/*document.getElementById('mobile-menu').onclick = function() {
+    document.getElementById('mobile-menu-content').style.display='block';
+}*/
+
+/*function openNav() {
+    document.getElementById("mobile-menu-conten").style.display = "block";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }*/
+
+  /*function openNav() {
+    const element = document.getElementById("mobile-menu-content");
+    element.classList.toggle("menu-show");
+  }*/
